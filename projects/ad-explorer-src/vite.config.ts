e7890b143e2b6c51e9d5c679f6cab9b818eb6224
Grayson Gooden <gygooden@gmail.com>
2025-11-21 01:29:48 -0500

    Update vite config for AD Explorer deploy

--- projects/ad-explorer-src/vite.config.ts
+++ projects/ad-explorer-src/vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/projects/ad-explorer/dist/',   // using dist from build
+  base: '/projects/ad-explorer/',
 })
